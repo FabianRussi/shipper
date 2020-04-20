55edfc9ac41d949a2188c5298fb00fd7a17c358f
jesus <jesu.miguez@suitedb.com>
2020-04-20 18:58:12 -0300

    comitear para no perder cambios

---
+++ Modules/elimsolutions/CreateSalesOrders@1.0.0/JavaScript/CreateSalesOrders.Model.js
@@ -0,0 +1,10 @@
+define('CreateSalesOrders.Model', ['Backbone'], function(
+    Backbone
+) {
+    'use strict';
+    debugger;
+    ///app/site/hosting/scriptlet.nl?script=customscript_es_get_curr_inv_items&deploy=1&esID=640
+    return Backbone.Model.extend({
+        urlRoot: 'services/CurrentInventory.service.ss'
+    });
+});
---
+++ Modules/elimsolutions/CreateSalesOrders@1.0.0/SuiteScript/CreateSalesOrders.Model.js
@@ -0,0 +1,37 @@
+define('CreateSalesOrders.Model', [
+    'SC.Model',
+    'Application'
+], function(SCModel, Application) {
+    'use strict';
+
+    return SCModel.extend({
+        name: 'CreateSalesOrders',
+        getCustomerId: function() {
+            // get id of logged in shopper and set as search criteria
+            var customerFields = customer.getFieldValues(['internalid']);
+            console.log('Customer Fields', JSON.stringify(customerFields));
+            var customerId = customerFields.internalid;
+            return customerId;
+        }
+
+        ,
+        listAll: function() {
+
+            try {
+                var customerId = this.getCustomerId();
+                var url = "https://5445214-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=617&deploy=1&compid=5445214_SB1&h=64af2839ca4fe4463c4f&esID=" + customerId;
+                console.log('URL', url);
+                var a = { "User-Agent-x": "SuiteScript-Call" };
+                var response = nlapiRequestURL(url, null, a, 'GET');
+                var body = response.getBody();
+                var body = body.replace(/\r?\n|\r/g, '');
+
+                return JSON.parse(body);
+
+            } catch (err) {
+                console.log('JSONERR', JSON.stringify(err));
+            }
+        }
+
+    });
+});
