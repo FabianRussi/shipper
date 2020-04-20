b83b9535bff4cb924e5769d9d62e38791112710d
Federico AT <fede.testoni@suitedb.com>
2020-04-20 16:45:00 -0300

    added harcorded test object to router

--- Modules/elimsolutions/CurrentInventory@1.0.0/JavaScript/CurrentInventory.Router.js
+++ Modules/elimsolutions/CurrentInventory@1.0.0/JavaScript/CurrentInventory.Router.js
@@ -75,5 +75,49 @@ define('CurrentInventory.Router'
                 this.collection.on('reset', view.showContent, view);
                 view.showContent();
             }
+
+            , itemDetails: function (id) {
+                // var params_options = _.parseUrlOptions(options);
+                var model = {}; //new CurrentInventoryItemDetailModel();
+                model.internalId = 19213;
+                model.name = 'BE 10150';
+                model.description = 'Item description';
+                model.type = 'Item type';
+                model.basePrice = 300;
+                model.height = 30;
+                model.weight = 4.5;
+                model.location = 'Item location';
+                model.reorderPoint = 'Item reorder point';
+                model.serial = 'Item serial';
+                model.onHand = 'Item on hand';
+                model.available = 'Item available';
+                model.stockUnit = 'Item stockUnit';
+                model.binNumber = 'Item binNumber';
+                model.status = 'Item status';
+                model.quantity = 'Item quantity';
+                model.expirationDate = 'Item expirationDate';
+                model.locationDetails = 'Item locationDetails';
+                model.locationCommited = 'Item locationCommited';
+
+                var view = new CurrentInventoryItemDetailView({
+                    application: this.application
+                    , id: id
+                    , model: model
+                });
+
+                view.showContent();
+
+                // var item = new WebOrderApprovalPendingApprovalCollection();
+                /*  var view = new WebOrderApprovalPendingApprovalListView({
+                     collection: collection,
+                     application: this.application,
+                     page: params_options.page,
+                     options: params_options
+                 });
+                 view.showContent();
+  */
+            }
+
+            //https://customercentersecure.eshipperplus.com/portal/my_account-local.ssp/item-details/view/4108
         });
     });
